e1220205dd6c7f5570669957ae73e704d251b5dd
Bao Nguyen <bao@fastly.com>
2014-12-02 13:38:56 -0800

    initial attempt at expression parser

---
+++ lib/node_extensions.rb
@@ -0,0 +1,67 @@
+# This module contains our custom syntax node classes. Each
+# class represents a distinct type of entity. Each clas
+# also has a distinct to_array method which allows the
+# final AST to be converted easily into a structured array
+# representatiion
+
+module CLI
+  class IntegerLiteral < Treetop::Runtime::SyntaxNode
+    def to_array
+      return self.text_value.to_i
+    end
+  end
+
+  class Username < Treetop::Runtime::SyntaxNode
+    def to_array
+      return eval self.text_value
+    end
+  end
+
+  class Role < Treetop::Runtime::SyntaxNode
+    def to_array
+      return eval self.text_value
+    end
+  end
+
+  class Secret  < Treetop::Runtime::SyntaxNode
+    def to_array
+      return eval self.text_value
+    end
+  end
+
+  class SSHKey < Treetop::Runtime::SyntaxNode
+    def to_array
+      return eval self.text_value
+    end
+  end
+
+  class FloatLiteral < Treetop::Runtime::SyntaxNode
+    def to_array
+      return self.text_value.to_f
+    end
+  end
+
+  class Identifier < Treetop::Runtime::SyntaxNode
+    def to_array
+      return self.text_value.to_sym
+    end
+  end
+
+  class Expression < Treetop::Runtime::SyntaxNode
+    def to_array
+      return self.elements[0].to_array
+    end
+  end
+
+  class Body < Treetop::Runtime::SyntaxNode
+    def to_array
+      return self.elements.map {|x| x.to_array}
+    end
+  end
+
+  class WordLiteral < Treetop::Runtime::SyntaxNode
+    def to_array
+      return self.text_value.to_sym
+    end
+  end
+end
